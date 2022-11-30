74813298d45e3d769a5b9a4dacba64388465d21f
szaszgergo <71765638+szaszgergo@users.noreply.github.com>
2022-11-30 19:37:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const minutes= document.getElementById('minutes');
 const seconds= document.getElementById('seconds');
 
 const currentYear = new Date().getFullYear();
-const newYearTime = new Date(`January 1 ${ currentYear + 1 } 00:00:00`);
+const newYearTime = new Date(`January 19 ${ currentYear + 1 } 00:00:00`);
 
 
 function updateCountdown(){
